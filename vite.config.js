dc8d8f8c825b2d8fd6eef80d1285429397bf7f92
sakimotorin <sakimotorin@gmail.com>
2024-08-19 14:33:14 +0800

    update router path

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/30-days-vue/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
